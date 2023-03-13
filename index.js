31f75c006c3850104b9b0d2298851357aa79d53b
Hacca13 <V.Noviello3@gmail.com>
2023-03-13 17:38:58 +0100

    init project

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const path = require('path');
+
+require("./auth")
+
+const app = express()
+app.use(session({ secret: 'cats' }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+const port = 5000
+
+function isLoggedIn(req, res, next){
+    req.user ? next() : res.sendStatus(401)
+}
+// '<a href="/auth/google"> Authenticate with Google</a>'
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index2.html')))
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {scope: ['email', 'profile']})
+)
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/protected',
+        failureRedirect: '/auth/failure'
+    })
+)
+
+app.get('/auth/failure', (req, res) => res.send('Non sei autorizzato'))
+//res.send(`Hello ${req.user.displayName}`)
+app.get('/protected', isLoggedIn, (req, res) => res.sendFile(path.join(__dirname, '/public/successLogin.html')))
+
+app.get('/logout', (req, res) => {
+    
+    req.session.destroy()
+    res.send('Goodbye!')
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
